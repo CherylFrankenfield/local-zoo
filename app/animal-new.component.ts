e1697213dedbe219966cdb94cf8c9d25bb3a47d5
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-01-21 10:43:32 -0800

    Add styles to AddAnimal component form

--- app/animal-new.component.ts
+++ app/animal-new.component.ts
@@ -4,27 +4,30 @@ import { Animal } from './animal.model';
 @Component ({
   selector: 'animal-new',
   template: `
-    <h1>Animal Intake</h1>
+    <h2>Animal Intake</h2>
+    <p>Add new animal to database.<p>
     <form>
-      <label name="species">Species Name</label>
-      <input #newSpecies name="species"><br>
-      <label name="name">Animal Name</label>
-      <input #newName name="name"><br>
-      <label name="age">Animal Age</label>
-      <input #newAge name="age"><br>
-      <label name="diet">Diet</label>
-      <input #newDiet name="diet"><br>
-      <label name="location">Zoo Location</label>
-      <input #newLocation name="location"><br>
-      <label name="caretakers"># of Caretakers</label>
-      <input #newCaretakers name="caretakers"><br>
-      <label name="sex">Sex of Animal</label>
-      <input #newSex name="sex"><br>
-      <label name="likes">Animal Likes</label>
-      <input #newLikes name="likes"><br>
-      <label name="dislikes">Animal Dislikes</label>
-      <input #newDislikes name="dislikes"><br>
-      <button class="btn btn-outline-secondary" (click)="submitForm(newSpecies.value, newName.value, newAge.value, newDiet.value, newLocation.value, newCaretakers.value, newSex.value, newLikes.value, newDislikes.value); newSpecies.value=''; newName.value=''; newAge.value=''; newDiet.value=''; newLocation.value=''; newCaretakers.value=''; newSex.value=''; newLikes.value=''; newDislikes.value='';">Add Animal</button>
+      <div class="form-group">
+        <label name="species">Species Name</label>
+        <input #newSpecies name="species"><br>
+        <label name="name">Animal Name</label>
+        <input #newName name="name"><br>
+        <label name="age">Animal Age</label>
+        <input #newAge name="age"><br>
+        <label name="diet">Diet</label>
+        <input #newDiet name="diet"><br>
+        <label name="location">Zoo Location</label>
+        <input #newLocation name="location"><br>
+        <label name="caretakers"># of Caretakers</label>
+        <input #newCaretakers name="caretakers"><br>
+        <label name="sex">Sex of Animal</label>
+        <input #newSex name="sex"><br>
+        <label name="likes">Animal Likes</label>
+        <input #newLikes name="likes"><br>
+        <label name="dislikes">Animal Dislikes</label>
+        <input #newDislikes name="dislikes"><br>
+      </div>
+      <button class="btn btn-outline-secondary" (click)="submitForm(newSpecies.value, newName.value, newAge.value, newDiet.value, newLocation.value, newCaretakers.value, newSex.value, newLikes.value, newDislikes.value); newSpecies.value=''; newName.value=''; newAge.value=''; newDiet.value=''; newLocation.value=''; newCaretakers.value=''; newSex.value=''; newLikes.value=''; newDislikes.value='';">ADD ANIMAL</button>
     </form>
   `
 })
